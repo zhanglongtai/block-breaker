944b4b4e0bea4f7e4af37b4c3d935919a8b90c37
zhanglongtai <neo_seele@hotmail.com>
2018-10-26 16:01:02 +0800

    1-1 version

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const log = console.log.bind(console)
+
 const imgFromPath = function(path) {
     const img = new Image()
     img.src = path
@@ -11,8 +13,8 @@ const Paddle = function() {
     const p = {
         image: image,
         x: 100,
-        y: 200,
-        speed: 5,
+        y: 250,
+        speed: 15,
     }
 
     p.moveLeft = function() {
@@ -23,36 +25,67 @@ const Paddle = function() {
         p.x += p.speed
     }
 
+    p.collide = function(ball) {
+        if (ball.y + ball.image.height > p.y) {
+            if (ball.x > p.x && ball.x < p.x + p.image.width) {
+                log('相撞')
+                return true
+            }
+        }
+
+        return false
+    }
+
     return p
+}
+
+const Ball = function() {
+    const image = imgFromPath('ball.png')
+
+    const b = {
+        image: image,
+        x: 100,
+        y: 220,
+        speedX: 10,
+        speedY: 10,
+        fired: false,
+    }
+
+    b.fire = function() {
+        b.fired = true
+    }
+
+    b.move = function() {
+        if (b.fired) {
+            if (b.x < 0 || b.x > 400) {
+                b.speedX = -b.speedX
+            }
+    
+            if (b.y < 0 || b.y > 300) {
+                b.speedY = -b.speedY
+            }
+    
+            b.x += b.speedX
+            b.y += b.speedY
+        }
+    }
+
+    return b
 }
 
 const Game = function() {
     const canvas = document.querySelector('#id-game-interface')
-    const ctx = this.canvas.getContext('2d')
-
-
-    // timer
-    setInterval(function() {
-        // event
-        const keyList = Object.keys(this.keydowns)
-        for (const key of keyList) {
-            if (this.keydowns[key]) {
-                this.actions[key]()
-            }
-        }
-        // update
-        g.update()
-        // clear
-        ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight)
-        // draw
-        g.draw()
-    }, 1000/30)
+    const ctx = canvas.getContext('2d')
 
     const g = {
         canvas: canvas,
         context: ctx,
         actions: {},
         keydowns: {},
+    }
+
+    g.drawImage = function(imgObj) {
+        g.context.drawImage(imgObj.image, imgObj.x, imgObj.y)
     }
 
     window.addEventListener('keydown', (event) => {
@@ -64,8 +97,25 @@ const Game = function() {
     })
 
     g.registerAction = function(keyName, callback) {
-        this.actions[keyName] = callback
+        g.actions[keyName] = callback
     }
+
+    // timer
+    setInterval(function() {
+        // event
+        const keyList = Object.keys(g.actions)
+        for (const key of keyList) {
+            if (g.keydowns[key]) {
+                g.actions[key]()
+            }
+        }
+        // update
+        g.update()
+        // clear
+        g.context.clearRect(0, 0, g.canvas.clientWidth, g.canvas.clientHeight)
+        // draw
+        g.draw()
+    }, 1000/30)
 
     return g
 }
@@ -73,11 +123,8 @@ const Game = function() {
 const __main = function() {
     const game = Game()
     const paddle = Paddle()
+    const ball = Ball()
     
-    game.draw = function() {
-        game.context.drawImage(paddle.image, paddle.x, paddle.y)
-    }
-
     game.registerAction('ArrowLeft', () => {
         paddle.moveLeft()
     })
@@ -85,6 +132,22 @@ const __main = function() {
     game.registerAction('ArrowRight', () => {
         paddle.moveRight()
     })
+
+    game.registerAction(' ', () => {
+        ball.fire()
+    })
+
+    game.draw = function() {
+        game.drawImage(paddle)
+        game.drawImage(ball)
+    }
+
+    game.update = function() {
+        ball.move()
+        if (paddle.collide(ball)) {
+            ball.speedY = -ball.speedY
+        }
+    }
 }
 
 __main()
